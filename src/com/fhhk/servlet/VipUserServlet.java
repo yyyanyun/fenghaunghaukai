f8450b47dd6eb9b20af04f9073734279ff5c266b
xiaowang <1738546154@qq.com>
2023-04-21 19:40:24 +0800

    新增查询 单个删除 批量删除 修改会员信息

--- src/com/fhhk/servlet/VipUserServlet.java
+++ src/com/fhhk/servlet/VipUserServlet.java
@@ -29,4 +29,54 @@ public class VipUserServlet extends BaseServlet{
         }
         JsonUtils.toJson(rs,resp);
     }
+    public void updatetVipUser(HttpServletRequest req, HttpServletResponse resp){
+        String customerId = req.getParameter("customer_id");
+        String vipLevel = req.getParameter("vip_level");
+        String discount = req.getParameter("discount");
+        String freeCount = req.getParameter("free_count");
+        String vipMoney = req.getParameter("vip_money");
+        String vipCreatedate = req.getParameter("vip_createdate");
+        VipUser vu=new VipUser();
+        vu.setVip_createdate(vipCreatedate);
+       vu.setDiscount(Double.parseDouble(discount));
+       vu.setVip_level(vipLevel);
+       vu.setVip_money(Double.parseDouble(vipMoney));
+       vu.setCustomer_id(Integer.parseInt(customerId));
+       vu.setFree_count(Integer.parseInt(freeCount));
+       VipUseService vi=new VipUserServiceimpl();
+       ResultVo<Boolean> re=new ResultVo<>();
+        Integer updatevip = vi.updatevip(vu);
+        if (updatevip>0) {
+            re.setData(true);
+            re.setCode(200);
+            re.setMessage("成功");
+
+        }else {
+            re.setData(false);
+            re.setCode(500);
+            re.setMessage("失败");
+        }
+        JsonUtils.toJson(re,resp);
+
+
+    }
+    public void delectVip(HttpServletRequest req, HttpServletResponse resp){
+        String tid = req.getParameter("tid");
+        VipUseService vs=new VipUserServiceimpl();
+        Integer integer = vs.delectVip(tid);
+        ResultVo<Boolean> rs=new ResultVo();
+        if (integer >0) {
+            rs.setCode(200);
+            rs.setData(true);
+            rs.setMessage("成功");
+        }else {
+            rs.setMessage("失败");
+            rs.setData(false);
+            rs.setCode(200);
+        }
+        JsonUtils.toJson(rs,resp);
+
+
+
+    }
 }
