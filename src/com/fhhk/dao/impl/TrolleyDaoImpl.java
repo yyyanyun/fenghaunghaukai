84254b718f1119509cadb7a2255a282048abbd04
lsl <2091924566@qq.com>
2023-04-21 16:26:38 +0800

    新添加了一个分支lsl

---
+++ src/com/fhhk/dao/impl/TrolleyDaoImpl.java
@@ -0,0 +1,12 @@
+package com.fhhk.dao.impl;
+
+/**
+ * @BelongsProject:fenghaunghaukai
+ * @BelongsPackage:com.fhhk.dao.impl
+ * @Author:LSL
+ * @CreateTime:2023/4/21
+ * @Description:TODO
+ * @Version: 1.0
+ */
+public class TrolleyDaoImpl {
+}
---
+++ src/com/fhhk/entity/Trolley.java
@@ -0,0 +1,19 @@
+package com.fhhk.entity;
+
+import java.io.Serializable;
+
+/**
+ * @BelongsProject:fenghaunghaukai
+ * @BelongsPackage:com.fhhk.entity
+ * @Author:LSL
+ * @CreateTime:2023/4/21
+ * @Description:TODO
+ * @Version: 1.0
+ */
+public class Trolley implements Serializable {
+    private int tid;
+    private int customer_id;//用户id
+    private int service_id;//服务id
+    private int trolley_number;
+
+}
